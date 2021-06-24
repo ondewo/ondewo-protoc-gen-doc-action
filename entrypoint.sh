4120f88064c9db0e5cd106b17c0e1fa91419c102
Filip Povolny <filip.povolny@ondewo.com>
2021-06-24 11:21:08 +0200

    Add html template.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -l
 
+FORMAT=${1}
+OUTFILE=${2}.${FORMAT}
 OUTDIR=doc
 mkdir -p ${OUTDIR}
-OUTFILE=${1}.md
-protoc -I. -Igoogleapis --doc_opt=/templates/custom-markdown.tmpl,${OUTFILE} --doc_out=${OUTDIR} $(find ondewo -name '*.proto' | sort)
+
+protoc -I. -Igoogleapis --doc_opt=/templates/${FORMAT}.tmpl,${OUTFILE} --doc_out=${OUTDIR} $(find ondewo -name '*.proto' | sort)
